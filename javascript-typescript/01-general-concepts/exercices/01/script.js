214cbfcd2010ffdd060e5d744261aff73b9731ee
mariatonicher <mariatonicher@users.noreply.github.com>
2023-11-03 22:24:42 +0000

    first class

---
+++ javascript-typescript/01-general-concepts/exercices/01/script.js
@@ -0,0 +1,28 @@
+let name = "Maria";
+console.log(name);
+
+let age = 25;
+console.log(age);
+
+let isEmployed = true;
+console.log(isEmployed);
+
+let trabalho = null;
+console.log(trabalho);
+
+let endereço;
+console.log(endereço);
+
+let nomeCompleto = "Maria" + " " + "Callapez";
+console.log(nomeCompleto);
+
+let mensagem = "o" + " " + "tempo" + " " + "voa";
+console.log(mensagem);
+
+let string3 = `${nomeCompleto}  ${mensagem}`;
+console.log(string3);
+
+let num1 = 45;
+let num2 = 35;
+let sum = num1 + num2;
+console.log(sum);
